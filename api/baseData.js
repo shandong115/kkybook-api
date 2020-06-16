7b6f125da8f789e800fc9bd8f6dc515ef3d18a4c
shandong115 <mr.danzhao@163.com>
2020-06-16 13:25:25 +0800

    ...

--- api/baseData.js
+++ api/baseData.js
@@ -5,7 +5,7 @@ function BaseData () {
 	this.setItems = function () {
 		let sqlStr = 'Select t1.book_id, t1.title, t1.author,t1.size, t2.img_path \
 					from book_meta t1, book_info t2 \
-					where t2.book_id=t1.remark order by t1.book_id desc';
+					where t1.isOk=1 and t2.book_id=t1.remark order by t1.book_id desc';
 		console.log(sqlStr)
 		query(sqlStr,(err, results, fields) => {
 			if(err) throw err;
